62f7c7d38f37f0baa8dd511fd7cfc1cca8b22e4f
Kyle Plummer <kyle.plummer@revature.com>
2023-12-04 10:25:12 -0500

    spring boot boilkerplate

---
+++ src/main/java/com/revature/Revagenda/RevagendaApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Revagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevagendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/Revagenda/RevagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Revagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
