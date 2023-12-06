3086c3357d3c476b964ce1b1520b28cafbc1adcd
Kyle Plummer <kyle.plummer@revature.com>
2023-12-06 13:52:18 -0500

    small cleanup

--- src/test/java/com/revature/Revagenda/RevagendaApplicationTests.java
+++ src/test/java/com/revature/Revagenda/RevagendaApplicationTests.java
@@ -1,13 +1,27 @@
 package com.revature.Revagenda;
 
+import com.revature.Revagenda.controllers.UserController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class RevagendaApplicationTests {
 
+	private ApplicationContext ac;
+
+	@BeforeEach
+	public void setup(@Autowired ApplicationContext ac) {
+		this.ac = ac;
+	}
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(ac.getBean(UserController.class));
 	}
 
 }
